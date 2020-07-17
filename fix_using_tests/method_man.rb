c6dd0e2478d3485fdb68f809139522ae6537ddfa
Melinda Diaz <melindadiaz@me.com>
2020-07-17 18:23:03 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,10 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def start_game(player1, player2)
-  binding.pry
+ 
   "Hello #{player1} & #{playar2}"
-  
+  binding.pry
+  puts "hello"
 end
 
 def play_game(player2)
